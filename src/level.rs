180a0cde20c795450bc014936538d74201763870
JL <jeremyli.gitemail@gmail.com>
2023-07-03 13:40:58 -0500

    organized code

---
+++ src/level.rs
@@ -0,0 +1,39 @@
+pub struct Level {
+	pub width: usize,
+	pub height: usize,
+	level_data: Vec<u8>,
+	pub spawnx: f64,
+	pub spawny: f64
+}
+
+impl Level {
+	pub fn new(w: usize, h: usize) -> Self {
+		Level {
+			width: w,
+			height: h,
+			level_data: vec![0u8; w * h],
+			spawnx: 0.0,
+			spawny: 0.0
+		}
+	}
+
+	pub fn out_of_bounds(&self, x: isize, y: isize) -> bool {
+		x < 0 || y < 0 || x >= self.width as isize || y >= self.height as isize	
+	}
+
+	pub fn get_tile(&self, x: isize, y: isize) -> u8 {
+		if self.out_of_bounds(x, y) {
+			return 0;
+		}
+
+		self.level_data[self.width * y as usize + x as usize]
+	}
+
+	pub fn set_tile(&mut self, x: isize, y: isize, tile: u8) {
+		if self.out_of_bounds(x, y) {
+			return;	
+		}
+
+		self.level_data[self.width * y as usize + x as usize] = tile;	
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,184 +1,72 @@
+use sdl2::EventPump;
 use sdl2::event::Event;
 use sdl2::pixels::Color;
 use sdl2::rect::{Rect, Point};
-use sdl2::keyboard::Keycode;
+use sdl2::keyboard::{KeyboardState, Scancode};
+use sdl2::mouse::MouseState;
 use std::time::Instant;
+use sdl2::video::Window;
+use sdl2::render::Canvas;
 
-enum Mode {
+mod level;
+mod raycast;
+use level::Level;
+use raycast::{raycast, Vector2f64};
+
+struct Camera {
+	pub position: Vector2f64,
+	pub rotation: f64,
+	pub speed: f64,
+	pub rotation_speed: f64,
+	pub fov: f64
+}
+
+impl Camera {
+	pub fn new(x: f64, y: f64, cam_rotation: f64, cam_fov: f64) -> Self {
+		Camera {
+			position: raycast::new_vector2(x, y),
+			rotation: cam_rotation,
+			fov: cam_fov,
+			speed: 0.0,
+			rotation_speed: 0.0
+		}
+	}
+
+	pub fn update(&mut self, dt: f64) {
+		self.position.x += self.speed * self.rotation.cos() * dt;
+		self.position.y += self.speed * self.rotation.sin() * dt;
+		self.rotation += self.rotation_speed * dt;
+	}
+
+	pub fn handle_key_input(&mut self, keyboard_state: &KeyboardState) {
+		if keyboard_state.is_scancode_pressed(Scancode::Up) {
+			self.speed = 2.0;	
+		} else if keyboard_state.is_scancode_pressed(Scancode::Down) {	
+			self.speed = -2.0;	
+		} else {
+			self.speed = 0.0;	
+		}
+
+		if keyboard_state.is_scancode_pressed(Scancode::Left) {
+			self.rotation_speed = -1.0;	
+		} else if keyboard_state.is_scancode_pressed(Scancode::Right) {	
+			self.rotation_speed = 1.0;	
+		} else {
+			self.rotation_speed = 0.0;	
+		}
+	}
+}
+
+#[derive(PartialEq, Eq, Clone, Copy)]
+enum GameMode {
 	Editor,
 	Game
 }
 
+#[derive(PartialEq, Eq, Clone, Copy)]
 enum ButtonState {
 	Released,
 	Held
-}
-
-struct Level {
-	width: usize,
-	height: usize,
-	level_data: Vec<u8>
-}
-
-impl Level {
-	fn new(w: usize, h: usize) -> Self {
-		Level {
-			width: w,
-			height: h,
-			level_data: vec![0u8; w * h]
-		}
-	}
-
-	fn out_of_bounds(&self, x: isize, y: isize) -> bool {
-		x < 0 || y < 0 || x >= self.width as isize || y >= self.height as isize	
-	}
-
-	fn get_tile(&self, x: isize, y: isize) -> u8 {
-		if self.out_of_bounds(x, y) {
-			return 0;
-		}
-
-		self.level_data[self.width * y as usize + x as usize]
-	}
-
-	fn set_tile(&mut self, x: isize, y: isize, tile: u8) {
-		if self.out_of_bounds(x, y) {
-			return;	
-		}
-
-		self.level_data[self.width * y as usize + x as usize] = tile;	
-	}
-}
-
-struct Raycast {
-	//Coordinates the ray hit
-    x: f64,
-    y: f64,
-	//Tile that was hit
-    tile_type: u8,
-}
-
-fn dist(x1: f64, y1: f64, x2: f64, y2: f64) -> f64 {
-	((x1 - x2).powi(2) + (y1 - y2).powi(2)).sqrt()
-}
-
-fn raycast(startx: f64,
-		   starty: f64,
-		   angle: f64,
-		   max_dist: f64,
-		   level: &Level) -> Raycast {
-    let mut vert = Raycast {
-        x: 0.0,
-        y: 0.0,
-        tile_type: 0,
-    };
-
-    //Check vertical lines
-    if angle.cos() > 0.0 {
-        let mut rayx = startx.ceil();
-        let mut rayy = (rayx - startx) * angle.tan() + starty;
-        while (startx - rayx).abs() < max_dist {
-            let xind = rayx as isize;
-            let yind = rayy.floor() as isize;
-
-            if !level.out_of_bounds(xind, yind) && 
-				level.get_tile(xind, yind) != 0
-            {
-                vert = Raycast {
-                    x: rayx,
-                    y: rayy,
-                    tile_type: level.get_tile(xind, yind),
-                };
-
-                break;
-            }
-
-            rayx += 1.0;
-            rayy += angle.tan();
-        }
-    } else if angle.cos() < 0.0 {
-        let mut rayx = startx.floor();
-        let mut rayy = (rayx - startx) * angle.tan() + starty;
-        while (startx - rayx).abs() < max_dist {
-            let xind = rayx as isize - 1;
-            let yind = rayy.floor() as isize;
-
-            if !level.out_of_bounds(xind, yind) &&
-				level.get_tile(xind, yind) != 0
-            {
-                vert = Raycast {
-                    x: rayx,
-                    y: rayy,
-                    tile_type: level.get_tile(xind, yind),
-                };
-                break;
-            }
-
-            rayx -= 1.0;
-            rayy -= angle.tan();
-        }
-    }
-
-    let mut horiz = Raycast {
-        x: 0.0,
-        y: 0.0,
-        tile_type: 0,
-    };
-
-    //Check horizontal lines
-    if angle.sin() > 0.0 {
-        let mut rayy = starty.ceil();
-        let mut rayx = (rayy - starty) * 1.0 / angle.tan() + startx;
-        while (starty - rayy).abs() < max_dist {
-            let xind = rayx.floor() as isize;
-            let yind = rayy as isize;
-
-            if !level.out_of_bounds(xind, yind) &&
-				level.get_tile(xind, yind) != 0
-            {
-                horiz = Raycast {
-                    x: rayx,
-                    y: rayy,
-                    tile_type: level.get_tile(xind, yind),
-                };
-                break;
-            }
-
-            rayy += 1.0;
-            rayx += 1.0 / angle.tan();
-        }
-    } else if angle.sin() < 0.0 {
-        let mut rayy = starty.floor();
-        let mut rayx = (rayy - starty) * 1.0 / angle.tan() + startx;
-        while (starty - rayy).abs() < max_dist {
-            let xind = rayx.floor() as isize;
-            let yind = rayy as isize - 1;
-
-            if !level.out_of_bounds(xind, yind) &&
-				level.get_tile(xind, yind) != 0
-            {
-                horiz = Raycast {
-                    x: rayx,
-                    y: rayy,
-                    tile_type: level.get_tile(xind, yind),
-                };
-                break;
-            }
-
-            rayy -= 1.0;
-            rayx -= 1.0 / angle.tan();
-        }
-    }
-
-    //Return the value that is closest
-    if (dist(horiz.x, horiz.y, startx, starty) < dist(vert.x, vert.y, startx, starty)
-        && horiz.tile_type != 0)
-        || vert.tile_type == 0
-    {
-        return horiz;
-    } else {
-        return vert;
-    }
 }
 
 fn invert_u8(val: u8) -> u8 {
@@ -187,6 +75,121 @@ fn invert_u8(val: u8) -> u8 {
 	}
 	
 	0
+}
+
+fn display_level(
+	canvas: &mut Canvas<Window>,
+	camera: &mut Camera,
+	level: &Level
+) -> Result<(), String> {
+	let mut angle = -camera.fov / 2.0 + camera.rotation;
+	for i in 0..800 {
+		let (hit, tile_type) = raycast(&camera.position, angle, 32.0, &level);
+		let d = (hit.x - camera.position.x) * camera.rotation.cos() +
+			(hit.y - camera.position.y) * camera.rotation.sin();
+					
+		if tile_type != 0 {
+			canvas.set_draw_color(Color::GREEN);
+			if hit.y.floor() == hit.y {		
+				canvas.set_draw_color(Color::RGB(0, 200, 0));
+			}
+
+			canvas.draw_line(Point::new(i, (-300.0 / d + 300.0) as i32), 
+										 Point::new(i, (300.0 / d + 300.0) as i32))
+				.map_err(|e| e.to_string())?;	
+		}
+
+		angle += camera.fov / 800.0;
+	}
+
+	Ok(())
+}
+
+fn display_level_editor(
+	canvas: &mut Canvas<Window>,
+	level: &Level
+) -> Result<(), String> {
+	for y in 0..level.height {
+		for x in 0..level.width {
+			canvas.set_draw_color(Color::WHITE);
+			canvas.draw_rect(Rect::new(x as i32 * 32, y as i32 * 32, 32, 32))
+				.map_err(|e| e.to_string())?;
+	
+			if level.get_tile(x as isize, y as isize) != 0 {
+				canvas.fill_rect(Rect::new(x as i32 * 32, y as i32 * 32, 32, 32))
+					.map_err(|e| e.to_string())?;	
+			}
+		}
+	}
+	
+	canvas.set_draw_color(Color::RED);
+	canvas.fill_rect(Rect::new(level.spawnx as i32 * 32, level.spawny as i32 * 32, 32, 32))
+		.map_err(|e| e.to_string())?;
+	Ok(())
+}
+
+fn handle_mouse_input_editor(
+	level: &mut Level,
+	mouse_state: &MouseState,
+	left_state: &ButtonState
+) -> ButtonState {
+	let mousex = (mouse_state.x() / 32) as isize;
+	let mousey = (mouse_state.y() / 32) as isize;
+
+	let new_left_state;
+
+	if mouse_state.left() {
+		if let ButtonState::Released = left_state {
+			level.set_tile(mousex, mousey, invert_u8(level.get_tile(mousex, mousey)));	
+		}
+	
+		new_left_state = ButtonState::Held;
+	} else {
+		new_left_state = ButtonState::Released;
+	}
+	
+	if mouse_state.right() {
+		level.spawnx = mousex as f64 + 0.5;
+		level.spawny = mousey as f64 + 0.5;
+	}
+
+	new_left_state
+}
+
+fn can_quit(event_pump: &mut EventPump) -> bool {
+	for event in event_pump.poll_iter() {
+		match event {
+			Event::Quit { .. } => { return true; }	
+			_ => {}	
+		}
+	}
+
+	false
+}
+
+fn switch_modes(
+	game_mode: &GameMode, 
+	p_key_state: &ButtonState,
+	keyboard_state: &KeyboardState,
+	camera: &mut Camera,
+	level: &Level
+) -> (ButtonState, GameMode) {
+	if keyboard_state.is_scancode_pressed(Scancode::P) &&
+	   *p_key_state == ButtonState::Released &&
+	   *game_mode == GameMode::Editor { 
+	   camera.position.x = level.spawnx;
+	   camera.position.y = level.spawny;
+	   camera.rotation = 0.0;
+	   return (ButtonState::Held, GameMode::Game);
+	} else if keyboard_state.is_scancode_pressed(Scancode::P) &&
+	   *p_key_state == ButtonState::Released &&
+	   *game_mode == GameMode::Game {
+	   return (ButtonState::Held, GameMode::Editor);
+	} else if keyboard_state.is_scancode_pressed(Scancode::P) {	
+	   return (ButtonState::Held, game_mode.clone());
+	}
+
+	(ButtonState::Released, game_mode.clone())
 }
 
 fn main() -> Result<(), String> {
@@ -204,137 +207,45 @@ fn main() -> Result<(), String> {
 	let mut event_pump = ctx.event_pump().map_err(|e| e.to_string())?;
 
 	let mut level = Level::new(16, 16);
-	
-	let mut left_state = ButtonState::Released;
-
-	let mut camx = 0.5f64;
-	let mut camy = 0.5f64;
-	let mut cam_rotation = 0.0f64;
-	let mut cam_rotation_speed = 0.0f64;
-	let mut cam_speed = 0.0f64;
-	const FOV: f64 = 3.14159 / 3.0;
-	
-	let mut mode = Mode::Editor;
-
+	let mut camera = Camera::new(0.5, 0.5, 0.0, 3.14159 / 3.0);
+	let mut left_state = ButtonState::Released;	
+	let mut p_key_state = ButtonState::Released;
+	let mut game_mode = GameMode::Editor;
 	let mut dt = 0.0f64;
 
-	'running: loop {
+	while !can_quit(&mut event_pump) {
 		let frame_start = Instant::now(); 
 
 		let mouse_state = event_pump.mouse_state();
+		let keyboard_state = event_pump.keyboard_state();
 
 		canvas.set_draw_color(Color::BLACK);
 		canvas.clear();
 		
-		match mode {
-			Mode::Editor => {
-				for y in 0..level.height {
-					for x in 0..level.width {
-						canvas.set_draw_color(Color::WHITE);
-						canvas.draw_rect(Rect::new(x as i32 * 32, y as i32 * 32, 32, 32))
-							.map_err(|e| e.to_string())?;
-	
-						if level.get_tile(x as isize, y as isize) != 0 {
-							canvas.fill_rect(Rect::new(x as i32 * 32, y as i32 * 32, 32, 32))
-								.map_err(|e| e.to_string())?;	
-						}
-					}
-				}
-	
-				canvas.set_draw_color(Color::RED);
-				canvas.fill_rect(Rect::new(camx as i32 * 32, camy as i32 * 32, 32, 32))
+		match game_mode {
+			GameMode::Editor => {
+				display_level_editor(&mut canvas, &level)
 					.map_err(|e| e.to_string())?;
-	
 				canvas.set_draw_color(Color::YELLOW);
 				canvas.draw_rect(Rect::new(mouse_state.x() / 32 * 32, mouse_state.y() / 32 * 32, 32, 32))
-					.map_err(|e| e.to_string())?;
-	
-				let mousex = (mouse_state.x() / 32) as isize;
-				let mousey = (mouse_state.y() / 32) as isize;
-				
-				if mouse_state.left() {
-					if let ButtonState::Released = left_state {
-						level.set_tile(mousex, mousey, invert_u8(level.get_tile(mousex, mousey)));	
-					}
-	
-					left_state = ButtonState::Held;
-				} else {
-					left_state = ButtonState::Released;
-				}
-	
-				if mouse_state.right() {
-					camx = mousex as f64 + 0.5;
-					camy = mousey as f64 + 0.5;
-				}
+					.map_err(|e| e.to_string())?;	
+				left_state = handle_mouse_input_editor(&mut level, &mouse_state, &left_state);
 			}	
-			Mode::Game => {
-				let mut angle = -FOV / 2.0 + cam_rotation;
-				for i in 0..800 {
-					let hit = raycast(camx, camy, angle, 32.0, &level);
-					let d = (hit.x - camx) * cam_rotation.cos() + (hit.y - camy) * cam_rotation.sin();
-					
-					if hit.tile_type != 0 {
-						canvas.set_draw_color(Color::GREEN);
-						if hit.y.floor() == hit.y {		
-							canvas.set_draw_color(Color::RGB(0, 200, 0));
-						}
-
-						canvas.draw_line(Point::new(i, (-300.0 / d + 300.0) as i32), 
-										 Point::new(i, (300.0 / d + 300.0) as i32))
-							.map_err(|e| e.to_string())?;
-	
-					}
-
-					angle += FOV / 800.0;
-				}
+			GameMode::Game => {
+				display_level(&mut canvas, &mut camera, &level)
+					.map_err(|e| e.to_string())?;	
+				camera.update(dt);
+				camera.handle_key_input(&keyboard_state);
 			}
 		}
+
+		(p_key_state, game_mode) = switch_modes(&game_mode,
+												&p_key_state,
+												&keyboard_state,
+												&mut camera,
+												&level);
 
 		canvas.present();	
-
-		for event in event_pump.poll_iter() {
-			match event {
-				Event::Quit { .. } => { break 'running; }
-				Event::KeyDown { keycode: Some(Keycode::P), repeat: false, .. } => {
-					if let Mode::Editor = mode {
-						mode = Mode::Game;	
-					} else {
-						mode = Mode::Editor;	
-					}
-				}
-				Event::KeyDown { keycode: Some(Keycode::Up), .. } => {
-					cam_speed = 2.0f64;	
-				}
-				Event::KeyDown { keycode: Some(Keycode::Down), .. } => {
-					cam_speed = -2.0f64;	
-				}
-				Event::KeyDown { keycode: Some(Keycode::Left), .. } => {
-					cam_rotation_speed = -1.0f64;	
-				}
-				Event::KeyDown { keycode: Some(Keycode::Right), .. } => {
-					cam_rotation_speed = 1.0f64;	
-				}
-				Event::KeyUp { keycode: Some(Keycode::Up), .. } => {
-					cam_speed = 0.0f64;	
-				}
-				Event::KeyUp { keycode: Some(Keycode::Down), .. } => {
-					cam_speed = 0.0f64;	
-				}
-				Event::KeyUp { keycode: Some(Keycode::Left), .. } => {
-					cam_rotation_speed = 0.0f64;	
-				}
-				Event::KeyUp { keycode: Some(Keycode::Right), .. } => {
-					cam_rotation_speed = 0.0f64;	
-				}
-				_ => {}	
-			}
-		}
-
-		if let Mode::Game = mode {
-			camx += cam_speed * cam_rotation.cos() * dt;
-			camy += cam_speed * cam_rotation.sin() * dt;
-			cam_rotation += cam_rotation_speed * dt;
-		}
 
 		dt = frame_start.elapsed().as_secs_f64();
 	}
